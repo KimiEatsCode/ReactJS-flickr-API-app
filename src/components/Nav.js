cf2d49ab1464103bfe26d2fef424e83cbc0c01e2
kimi <sarahkmcaleer@gmail.com>
2020-10-20 16:04:47 -0400

    Boilerplate for React app calling Flickr API

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import apiKey from '../config'
+import { NavLink } from 'react-router-dom';
+import SearchForm from './SearchForm';
+
+const Nav = props => (
+
+  <header>
+  	{ (props.location.pathname.startsWith('/search') )
+  		? <SearchForm props={props} apiKey={apiKey} />
+  		: <br /> }
+	<nav className="main-nav">
+	  <ul>
+		  <h1>test</h1>
+		<li><NavLink to='/search'>Search</NavLink></li>
+		<li><NavLink to='/cats'>Cats</NavLink></li>
+		<li><NavLink to='/dogs'>Dogs</NavLink></li>
+		<li><NavLink to='/birds'>Birds</NavLink></li>
+	  </ul>
+    </nav>
+  </header>
+);
+export default Nav;
